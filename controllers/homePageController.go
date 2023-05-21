4e1865b4f96ace6d5ad79424808121425a098cca
jijalidilikilijilia <starlex2002@mail.ru>
2023-05-21 21:02:17 +0300

    DAMN THAT'S VAJNO
    /home теперь отображает данные пользователя из базы данных

--- controllers/homePageController.go
+++ controllers/homePageController.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"kait_portfolio/database"
 	"net/http"
 
 	"github.com/gin-contrib/sessions"
@@ -8,14 +9,13 @@ import (
 )
 
 type userInfoToShow struct {
-	Username string
-	Age      int
-	// 	Group_name   string
-	// 	Cumpus_name  string
-	// 	Curator_name string
-	// 	Speciality   string
-	// 	About        string
-	Id uint
+	Full_name       string
+	Age             int
+	Group_name      string
+	Cumpus_name     string
+	Curator_name    string
+	Speciality_name string
+	Description     string
 }
 
 func HomePageGetController(ctx *gin.Context) {
@@ -32,19 +32,31 @@ func HomePagePostController(ctx *gin.Context) {
 
 func getUserData(ctx *gin.Context) userInfoToShow {
 	session := sessions.Default(ctx)
+	db := database.DB
+	var userInfo userInfoToShow
 
-	username := session.Get("username")
-	user_id := session.Get("user_id")
-	//user_id := session.Get("user_id")
+	user_id := session.Get("user_id").(uint)
 
-	if username == nil {
-		ctx.HTML(200, "error_page.html", nil)
+	err := db.Table("kait_portfolio.student").
+		Select("full_name", "age", "groups.group_name", "cumpus.cumpus_name", "specialities.speciality_name", "description").
+		Joins("JOIN kait_portfolio.groups ON student.group_id = groups.id").
+		Joins("JOIN kait_portfolio.cumpus ON student.cumpus_id = cumpus.id").
+		Joins("JOIN kait_portfolio.specialities ON student.speciality_id = specialities.id").
+		Where("student.id = ?", user_id).
+		Scan(&userInfo).Error
+
+	if err != nil {
+		ctx.Abort()
 	}
 
-	userInfo := userInfoToShow{
-		Username: username.(string),
-		Age:      21,
-		Id:       user_id.(uint),
+	userInfo = userInfoToShow{
+		Full_name:       userInfo.Full_name,
+		Age:             userInfo.Age,
+		Group_name:      userInfo.Group_name,
+		Cumpus_name:     userInfo.Cumpus_name,
+		Curator_name:    "TODO",
+		Speciality_name: userInfo.Speciality_name,
+		Description:     userInfo.Description,
 	}
 
 	return userInfo
